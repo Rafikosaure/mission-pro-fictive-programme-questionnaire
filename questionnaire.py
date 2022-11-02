07881484879ccb434718fc9728c9354a3d3647bf
Rafik Ben Sadi <rafikbensadi@live.fr>
2022-11-02 15:59:51 +0100

    Chargement du fichier json en ligne de commande

--- questionnaire.py
+++ questionnaire.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 class Question:
     def __init__(self, titre, choix, bonne_reponse):
@@ -57,6 +58,17 @@ class Questionnaire:
 
         return Questionnaire(questions, data["categorie"], data["titre"], data["difficulte"])
 
+    def from_json_file(filename):
+        try:
+            file = open(filename, "r")
+            json_data = file.read()
+            file.close()
+            questionnaire_data = json.loads(json_data)
+        except:
+            print("Exception lors de l'ouverture ou la lecture du fichier.")
+            return None
+        return Questionnaire.from_json_data(questionnaire_data)
+
     def lancer(self):
         score = 0
         nb_questions = len(self.questions)
@@ -76,15 +88,13 @@ class Questionnaire:
         return score
 
 
+# Questionnaire.from_json_file("animaux_leschats_debutant.json").lancer()
 
+if len(sys.argv) < 2:
+    print("ERREUR: Vous devez spécifier le nom du fichier json à charger.")
+    exit(0)
 
-# Charger un fichier JSON
-filename = "cinema_starwars_debutant.json"
-file = open(filename, "r")
-json_data = file.read()
-file.close()
-questionnaire_data = json.loads(json_data)
-
-Questionnaire.from_json_data(questionnaire_data).lancer()
-
-print()
+json_filename = sys.argv[1]
+questionnaire = Questionnaire.from_json_file(json_filename)
+if questionnaire:
+    questionnaire.lancer()
